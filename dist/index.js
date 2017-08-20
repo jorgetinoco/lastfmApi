6b37a2df85ca64a737e6ff6f43dd107c325f2e97
jorgetinoco <jorge.tinoco@wizeline.com>
2017-08-20 14:17:35 -0500

    add dist folder for heroku

---
+++ dist/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _swaggerJsdoc = require('swagger-jsdoc');
+
+var _swaggerJsdoc2 = _interopRequireDefault(_swaggerJsdoc);
+
+var _swaggerUiExpress = require('swagger-ui-express');
+
+var _swaggerUiExpress2 = _interopRequireDefault(_swaggerUiExpress);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+require('dotenv').config({ path: __dirname + '/config/.env' });
+
+var app = (0, _express2.default)();
+
+var swaggerDefinition = {
+  info: {
+    title: 'LastFm API',
+    version: '1.0.0',
+    description: 'LastFm API wrapper'
+  },
+  host: process.env.hostname,
+  basePath: '/'
+};
+
+var options = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./routes/*.js']
+};
+
+var swaggerSpec = (0, _swaggerJsdoc2.default)(options);
+
+app.use('/api-docs', _swaggerUiExpress2.default.serve, _swaggerUiExpress2.default.setup(swaggerSpec));
+
+require('./routes/routes.js')(app);
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://' + host + ':' + port);
+});
